5d7cab6ccf0d769523de79b539283d1ae0e466f7
Joel Lee <23797207@sun.ac.za>
2024-05-14 16:50:30 +0200

    Full clean

--- Group_36_Project_5/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.group_36_project_5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.demo to javafx.fxml;
-    exports org.example.demo;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# rw313_Project5
-A private repo for group 36 project 5
